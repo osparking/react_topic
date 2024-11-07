f6499bdb2b0e1debce1b52a655962a57611fbc84
osparking <jbpark03@gmail.com>
2024-11-07 20:48:42 +0900

    reducer function fully defined

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import { useReducer, useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const [state, dispatch] = useReducer(reducer, 0);
-  function reducer() {}
+  function reducer(state, action) {
+    if (action.type == "inc") {
+      return state + action.payload;
+    } else if (action.type == "dec") {
+      return state - action.payload;
+    }
+  }
   return (
     <div className="App">
-      {count}
+      계수: {count}
       <button onClick={() => setCount(count + 1)}>증가 1</button>
-      <button onClick={() => setCount(count - 1)}>감소 1</button><br/>    
-      {state}
+      <button onClick={() => setCount(count - 1)}>감소 1</button>
+      <br />
+      상태: {state}
     </div>
   );
 }
