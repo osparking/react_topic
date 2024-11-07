92b5b4a46dfa23dd2aa0541be93dfb1f57257c7f
osparking <jbpark03@gmail.com>
2024-11-07 20:17:36 +0900

    useReducer hook practice - phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const [state, dispatch] = useReducer(reducer, 0);
+  function reducer() {}
   return (
     <div className="App">
       {count}
       <button onClick={() => setCount(count + 1)}>증가 1</button>
-      <button onClick={() => setCount(count - 1)}>감소 1</button>
+      <button onClick={() => setCount(count - 1)}>감소 1</button><br/>    
+      {state}
     </div>
   );
 }
