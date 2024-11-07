9e31e85ae4e51b009985858edb0905c66a75e1ef
osparking <jbpark03@gmail.com>
2024-11-07 19:26:57 +0900

    add inc and dec buttons by 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">{count}</div>;
+  return (
+    <div className="App">
+      {count}
+      <button onClick={() => setCount(count + 1)}>증가 1</button>
+      <button onClick={() => setCount(count - 1)}>감소 1</button>
+    </div>
+  );
 }
 
 export default App;
