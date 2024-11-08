6fe86899f838d8a65091846bb73cbb5772a5ab48
osparking <jbpark03@gmail.com>
2024-11-08 09:54:19 +0900

    reducer return deals object

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ function App() {
   const [state, dispatch] = useReducer(reducer, { count: 0 });
   function reducer(state, action) {
     if (action.type == "inc") {
-      return state + action.payload;
+      return { ...state, count: state.count + action.payload };
     } else if (action.type == "dec") {
-      return state - action.payload;
+      return { ...state, count: state.count - action.payload };
     }
   }
   return (
     <div className="App">
-      상태: {state}
+      상태: {state.count}
       <button onClick={() => dispatch({ type: "inc", payload: 1 })}>
         상태 ^
       </button>
