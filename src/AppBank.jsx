c94aed5ba3d383e25c78d3bd85dd29f86fb297c7
osparking <jbpark03@gmail.com>
2024-11-08 15:08:17 +0900

    set money change to amount and check amount

--- src/AppBank.jsx
+++ src/AppBank.jsx
@@ -1,6 +1,10 @@
 import { useReducer, useState } from "react";
 export default function AppBank() {
-  function reducer() {}
+  function reducer(state, action) {
+    if (action.type === "money") {
+      return { amount: action.payload };
+    }
+  }
   const [balance, setBalance] = useState(0);
   const [state, dispatch] = useReducer(reducer, { amount: 0 });
 
@@ -9,10 +13,23 @@ export default function AppBank() {
       범이은행
       <p>잔고: {balance}</p>
       <div>
-        금액: <input type="number" value="0" />
+        금액:
+        <input
+          type="text"
+          value={state.amount}
+          onChange={(e) =>
+            dispatch({ type: "money", payload: Number(e.target.value) })
+          }
+        />
       </div>
       <div>
-        <button>입금</button>
+        <button
+          onClick={() => {
+            console.log(state.amount);
+          }}
+        >
+          입금
+        </button>
         <button>출금</button>
       </div>
     </div>
