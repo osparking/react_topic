c9fe42e9513e8c89a824dd67fb82b019f40af33c
osparking <jbpark03@gmail.com>
2024-11-08 20:10:30 +0900

    homepage route specified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
