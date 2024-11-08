14acd0641f3d8248d57120d2d67873c1044a091a
osparking <jbpark03@gmail.com>
2024-11-08 20:23:47 +0900

    route for dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
