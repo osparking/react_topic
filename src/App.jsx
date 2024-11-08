c7690b3bfbd66dbb580bc05c19a77a1c28c269bd
osparking <jbpark03@gmail.com>
2024-11-08 20:37:02 +0900

    for non-existing path request

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,3 @@
+export default function PageNotFound() {
+  return <div>존재하지 않는 페이지이다.</div>;
+}
