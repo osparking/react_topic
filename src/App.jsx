4c2e4a2e15264aabf7622685d1ab39e22dae7336
osparking <jbpark03@gmail.com>
2024-11-09 08:58:50 +0900

    put Nav compo into App compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Homepage from "./pages/Homepage";
+import Nav from "./pages/Nav";
 import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/dashboard" element={<Dashboard />} />
