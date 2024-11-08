cb18b9d845654cd2a02c5b3d322f523936b856b5
osparking <jbpark03@gmail.com>
2024-11-08 15:54:53 +0900

    amount: state, balance: reducer, impl withd'

--- src/AppBank.jsx
+++ src/AppBank.jsx
@@ -1,36 +1,34 @@
 import { useReducer, useState } from "react";
 export default function AppBank() {
   function reducer(state, action) {
-    if (action.type === "money") {
-      return { amount: action.payload };
+    if (action.type === "withdraw") {
+      return { balance: state.balance - amount };
     }
   }
-  const [balance, setBalance] = useState(0);
-  const [state, dispatch] = useReducer(reducer, { amount: 0 });
+  const [amount, setAmount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { balance: 0 });
 
   return (
     <div>
       범이은행
-      <p>잔고: {balance}</p>
+      <p>잔고: {state.balance}</p>
       <div>
         금액:
         <input
           type="text"
-          value={state.amount}
-          onChange={(e) =>
-            dispatch({ type: "money", payload: Number(e.target.value) })
-          }
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
         />
       </div>
       <div>
         <button
           onClick={() => {
-            console.log(state.amount);
+            console.log(state.balance);
           }}
         >
           입금
         </button>
-        <button>출금</button>
+        <button onClick={() => dispatch({ type: "withdraw" })}>출금</button>
       </div>
     </div>
   );
