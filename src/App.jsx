a6e0ca180ea5fad0d9213ae55ad079a5a47f0c73
osparking <jbpark03@gmail.com>
2024-11-08 19:39:16 +0900

    simplified application start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,5 @@
-import { useReducer } from "react";
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0, delta: 1 });
-
-  function reducer(state, action) {
-    if (action.type == "inc") {
-      return { ...state, count: state.count + state.delta };
-    } else if (action.type == "dec") {
-      return { ...state, count: state.count - state.delta };
-    } else if (action.type == "delta") {
-      return { ...state, delta: action.payload };
-    }
-  }
-  return (
-    <div className="App">
-      <div>
-        변화량:
-        <input
-          type="text"
-          value={state.delta}
-          onChange={(e) =>
-            dispatch({ type: "delta", payload: Number(e.target.value) })
-          }
-        />
-      </div>
-      상태: {state.count}
-      <button onClick={() => dispatch({ type: "inc" })}>상태 ^</button>
-      <button onClick={() => dispatch({ type: "dec" })}>상태 v</button>
-    </div>
-  );
+  return <div>이것은 응용 (대문) 페이지이다.</div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppBank from "./AppBank";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppBank />
+    <App />
   </React.StrictMode>
 );
