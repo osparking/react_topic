5d4ebc8cf3012d447bb78ca1776bc1e52edcf16b
osparking <jbpark03@gmail.com>
2024-11-09 08:58:05 +0900

    remove links from each compo's

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,3 @@
-import { Link } from "react-router-dom";
 export default function Dashboard() {
-  return (
-    <div>
-      <Link to="/">홈페이지</Link>
-      이것은 대쉬보드이다.
-    </div>
-  );
+  return <div>이것은 대쉬보드이다.</div>;
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,3 @@
-import { Link } from "react-router-dom";
 export default function Homepage() {
-  return (
-    <div>
-      <Link to="/dashboard">대시보드</Link>
-      이것은 홈페이지이다.
-    </div>
-  );
+  return <div>이것은 홈페이지이다.</div>;
 }
