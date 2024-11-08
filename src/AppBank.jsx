c29bb9c549a8928d515cbb96b031d96fa93a8556
osparking <jbpark03@gmail.com>
2024-11-08 14:39:55 +0900

    useState, useReducer for balance, amount

--- src/AppBank.jsx
+++ src/AppBank.jsx
@@ -1,3 +1,20 @@
+import { useReducer, useState } from "react";
 export default function AppBank() {
-  return <div>범이은행</div>;
+  function reducer() {}
+  const [balance, setBalance] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { amount: 0 });
+
+  return (
+    <div>
+      범이은행
+      <p>잔고: {balance}</p>
+      <div>
+        금액: <input type="number" value="0" />
+      </div>
+      <div>
+        <button>입금</button>
+        <button>출금</button>
+      </div>
+    </div>
+  );
 }
