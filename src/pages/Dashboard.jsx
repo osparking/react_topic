5751ab1d8878db372b4425d16236cff4d68bf7d8
osparking <jbpark03@gmail.com>
2024-11-09 21:49:36 +0900

    links became list items

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,9 +2,14 @@ import { Link, Outlet } from "react-router-dom";
 export default function Dashboard() {
   return (
     <div>
-      <Link to="profile">프로필</Link>&nbsp;
-      <Link to="settings">설정</Link>
-      <br />
+      <ul>
+        <li>
+          <Link to="profile">프로필</Link>
+        </li>
+        <li>
+          <Link to="settings">설정</Link>
+        </li>
+      </ul>
       이것은 대쉬보드이다.
       <Outlet />
     </div>
