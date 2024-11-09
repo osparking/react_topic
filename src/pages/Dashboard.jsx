bc3e0e0ef6e453e5604226f407f591e4fdec830c
osparking <jbpark03@gmail.com>
2024-11-09 21:29:51 +0900

    outlet shows nested route path by path

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,9 @@
+import { Outlet } from "react-router-dom";
 export default function Dashboard() {
-  return <div>이것은 대쉬보드이다.</div>;
+  return (
+    <div>
+      이것은 대쉬보드이다.
+      <Outlet />
+    </div>
+  );
 }
