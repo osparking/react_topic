46eb753eb154adca0677c6e12c71512a7b8142f8
osparking <jbpark03@gmail.com>
2024-11-09 21:15:52 +0900

    nested routing processing for settings

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Dashboard from "./pages/Dashboard";
 import Homepage from "./pages/Homepage";
 import Nav from "./pages/Nav";
 import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/settings" element={<Settings />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
