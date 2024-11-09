4a36bc5714fa03679daa59c5fe76df3e2f38b6d0
osparking <jbpark03@gmail.com>
2024-11-09 21:41:27 +0900

    line compo free us from typing URL

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 export default function Dashboard() {
   return (
     <div>
+      <Link to="profile">프로필</Link>&nbsp;
+      <Link to="settings">설정</Link>
+      <br />
       이것은 대쉬보드이다.
       <Outlet />
     </div>
