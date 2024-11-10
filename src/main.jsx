ad40d8789418ad442a832295fed66daa9214c10b
osparking <jbpark03@gmail.com>
2024-11-10 14:57:50 +0900

    AppContext compo' initial version

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import AppContext from "./pages/AppContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppContext />
   </React.StrictMode>
 );
---
+++ src/pages/AppContext.jsx
@@ -0,0 +1,5 @@
+function AppContext() {
+  return <div></div>;
+}
+
+export default AppContext;
