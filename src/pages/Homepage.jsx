520ffa548a46ac86ec37f9a8c9a5e15cd7d40028
osparking <jbpark03@gmail.com>
2024-11-10 11:49:36 +0900

    programmatic navigation to other location

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Homepage() {
-  return <div>이것은 홈페이지이다.</div>;
+  const navigate = useNavigate();
+  function readProfile() {
+    navigate("/dashboard/profile");
+  }
+  return (
+    <div>
+      이것은 홈페이지이다.<button onClick={readProfile}>프로필 보기</button>
+    </div>
+  );
 }
