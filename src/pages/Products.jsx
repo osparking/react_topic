1840c33f6028c51660b6f385d593fea16ca0101e
osparking <jbpark03@gmail.com>
2024-11-10 09:02:56 +0900

    destruct 'id' key from params using useParams

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom";
+
 export default function Products() {
+  const { id } = useParams();
   return (
     <div>
       <p>제품 상세 정보</p>
-      <p>표시되는 제품 ID: ?</p>
+      <p>표시되는 제품 ID: {id}</p>
     </div>
   );
 }
