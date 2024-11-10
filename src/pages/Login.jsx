e89d7630a938d7b52a9ea3ad1b198d47faf6495d
osparking <jbpark03@gmail.com>
2024-11-10 21:52:26 +0900

    update context using function provided

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { userContext } from "./AppContext";
 export default function Login() {
   const [userId, setUserId] = useState("");
+  const { user, setUser } = useContext(userContext);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setUser(userId);
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={userId}
