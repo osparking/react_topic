8a60825e7ea8e68f7245dd9f49785f230785c341
osparking <jbpark03@gmail.com>
2024-11-10 21:02:52 +0900

    Login compo used instead of input tag

--- src/pages/AppContext.jsx
+++ src/pages/AppContext.jsx
@@ -1,16 +1,13 @@
 import { createContext, useState } from "react";
 import CheckOut from "./CheckOut";
+import Login from "./Login";
 export const userContext = createContext();
 
 function AppContext() {
   const [user, setUser] = useState("guest");
   return (
     <div>
-      <input
-        type="text"
-        value={user}
-        onChange={(e) => setUser(e.target.value)}
-      />
+      <Login />
       <userContext.Provider value={{ user }}>
         <CheckOut />
       </userContext.Provider>
