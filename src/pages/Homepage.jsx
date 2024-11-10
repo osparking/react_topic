8ec9cd8e9adccbb20add89e6e5b741aff9020a1c
osparking <jbpark03@gmail.com>
2024-11-10 12:08:50 +0900

    get username from input box and pass it

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Homepage() {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
   function readProfile() {
-    navigate("/dashboard/profile");
+    navigate("/dashboard/profile", { state: { username } });
   }
   return (
     <div>
-      이것은 홈페이지이다.<button onClick={readProfile}>프로필 보기</button>
+      이것은 홈페이지이다.
+      <div>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={readProfile}>프로필 보기</button>
+      </div>
     </div>
   );
 }
