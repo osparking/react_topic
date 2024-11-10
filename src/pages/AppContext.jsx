d50d2c3ad19628bf7bd7a1d0a60151653d289f84
osparking <jbpark03@gmail.com>
2024-11-10 15:55:10 +0900

    passes 'user' state thru' context

--- src/pages/AppContext.jsx
+++ src/pages/AppContext.jsx
@@ -1,11 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import CheckOut from "./CheckOut";
 export const userContext = createContext();
 
 function AppContext() {
+  const [user, setUser] = useState("guest");
   return (
     <div>
-      <userContext.Provider value="guest">
+      <userContext.Provider value={{user}}>
         <CheckOut />
       </userContext.Provider>
     </div>
