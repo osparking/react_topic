442ac4ae0a9b9d05372d82d75589f8c7cd94aef1
osparking <jbpark03@gmail.com>
2024-11-12 10:34:58 +0900

    user reducer function defined

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,6 +1,16 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useReducer } from "react";
 const UserContext = createContext();
 function UserProvider({ children }) {
+  function reducer(state, action) {
+    switch (action.type) {
+      case "login":
+        return "";
+      case "logout":
+        return "";
+      default:
+        return state;
+    }
+  }
   const [user, dispatch] = useReducer(reducer, "");
   return (
     <UserContext.Provider value={{ user, setUser }}>
