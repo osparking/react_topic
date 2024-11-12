479f06236491f06fd8d9536cf2b84d3c2501ee0c
osparking <jbpark03@gmail.com>
2024-11-12 09:12:41 +0900

    read setUser from UserContext and use it

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 export default function Logout() {
+  const { setUser } = useContext(UserContext);
   return (
     <default>
-      <button>로그아웃</button>
+      <button onClick={() => setUser("guest")}>로그아웃</button>
     </default>
   );
 }
